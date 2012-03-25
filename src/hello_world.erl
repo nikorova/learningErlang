7478c2cf1f99558be85d8072dfbd6b6672d096a9
nikorova <nick@korovasoft.com>
2012-03-25 07:37:50 -0400

    guh

--- src/hello_world.erl
+++
@@ -1,5 +0,0 @@
--module(hello_world).
--export([hello_world/0]).
-
-hello_world() ->
-	io:fwrite("Hello world!\n").
--- src/pmatch.erl
+++
@@ -1,9 +0,0 @@
--module(pmatch).
--compile(export_all).
-
-head([H|_]) -> H.
-
-second([_, X|_]) -> X.
-
-same(X, X) -> true;
-same(_, _) -> false.
